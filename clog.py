4a5e75c29673e8e2132056a6714363bcad00a2f9
Antonio Zanardo <zanardo@gmail.com>
2013-07-14 08:15:25 -0300

    initial commit

---
+++ clog.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+import os.path
+import subprocess
+import uuid
+import time
+
+def gethome():
+	return os.path.expanduser('~')
+
+def getscriptspath():
+	return os.path.join(gethome(), '.clog-scripts')
+
+def getqueuepath():
+	return os.path.join(gethome(), '.clog-queue')
+
+def createpaths():
+	paths = [ getscriptspath(), getqueuepath() ]
+	for p in paths:
+		if not os.path.isdir(p):
+			print("creating {}...").format(p)
+			os.mkdir(p, 0700)
+
+def runqueue():
+	print("starting queue dispatch...")
+	qp = getqueuepath()
+	os.chdir(qp)
+	for f in os.listdir('.'):
+		if len(f) == 41 and f.endswith('.meta'):
+			rid = f[:36]
+			print("sending job {}...").format(rid)
+			os.unlink("{}.log".format(rid))
+			os.unlink("{}.meta".format(rid))
+	print("finished queue dispatch")
+
+def runscript(script):
+	print("running script {}").format(script)
+	sp = os.path.join(getscriptspath(), script)
+	if not os.path.isfile(sp):
+		print("error: file {} not found!").format(sp)
+		sys.exit(1)
+	rid = str(uuid.uuid4())
+	qlp = os.path.join(getqueuepath(), "{}.log".format(rid))
+	with open(qlp, "w") as fp:
+		st = time.time()
+		ret = subprocess.call(sp, stdout=fp, stderr=subprocess.STDOUT)
+		et = time.time()
+	qcp = os.path.join(getqueuepath(), rid)
+	with open(qcp + ".tmp", "w") as fp:
+		fp.write("start_time={}\n".format(st))
+		fp.write("end_time={}\n".format(et))
+		fp.write("status={}\n".format('OK' if ret == 0 else 'FAIL'))
+	os.rename(qcp + '.tmp', qcp + '.meta')
+	print("finished with id={}").format(rid)
+	return rid
+
+if __name__ == '__main__':
+
+	createpaths()
+
+	if len(sys.argv) == 1:
+		runqueue()
+	else:
+		unit = sys.argv[1]
+		rid = runscript(unit)
