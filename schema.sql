8f346097a69d1357e1569c0a78b6bbba9fc065d7
Antonio Zanardo <zanardo@gmail.com>
2013-10-20 18:42:25 -0200

    create schema for user authentication

--- schema.sql
+++ schema.sql
@@ -8,3 +8,18 @@ CREATE TABLE jobhistory (
 	datefinished decimal not null,
 	status integer not null
 );
+
+CREATE TABLE users (
+	username text NOT NULL PRIMARY KEY,
+	password text NOT NULL,
+	is_admin int
+);
+
+CREATE TABLE sessions (
+	session_id text NOT NULL PRIMARY KEY,
+	date_login timestamp NOT NULL DEFAULT ( datetime('now', 'localtime') ),
+	username text NOT NULL
+);
+
+INSERT INTO users ( username, password, is_admin )
+VALUES ( 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 1 );
