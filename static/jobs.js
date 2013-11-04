41cecd381cc45cc6e5bbcd82ef5681be1ee23b89
Antonio Zanardo <zanardo@gmail.com>
2013-11-03 22:04:48 -0200

    rename id so it should not move page on click (hash #jobs)

--- static/jobs.js
+++ static/jobs.js
@@ -1,15 +1,15 @@
 var screen;
 
 function updateScreen(jobs) {
-	document.getElementById('jobs').innerHTML = jobs;
-	document.getElementById('jobs').style.opacity = 1.0;
+	document.getElementById('jobstbl').innerHTML = jobs;
+	document.getElementById('jobstbl').style.opacity = 1.0;
 }
 
 function loadJobs() {
 	screen = 'jobs';
 	document.getElementById('btnloadjobs').style.fontWeight = 'bold';
 	document.getElementById('btnloadhistory').style.fontWeight = 'normal';
-	document.getElementById('jobs').style.opacity = 0.5;
+	document.getElementById('jobstbl').style.opacity = 0.5;
 	window.location.hash = '#jobs';
 	var req = new XMLHttpRequest();
 	req.open("GET", "/jobs", true);
@@ -25,7 +25,7 @@ function loadHistory() {
 	screen = 'history';
 	document.getElementById('btnloadhistory').style.fontWeight = 'bold';
 	document.getElementById('btnloadjobs').style.fontWeight = 'normal';
-	document.getElementById('jobs').style.opacity = 0.5;
+	document.getElementById('jobstbl').style.opacity = 0.5;
 	window.location.hash = '#history';
 	var req = new XMLHttpRequest();
 	req.open("GET", "/history", true);
